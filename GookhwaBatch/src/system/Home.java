5adf7b208e3ad180f2638f15a8630a6864af3ea6
yungiggs <yungiggs@gmail.com>
2016-08-30 04:01:23 +0900

    first commit

---
+++ GookhwaBatch/src/system/Home.java
@@ -0,0 +1,17 @@
+package system;
+
+public class Home {
+	
+	/*
+	 * 1.매시간 정각 타임 틱 발생 
+	 * 2.작업 준비 
+	 * 2-1 크롤링 돌린다.
+	 * 2-2 rdb 저장
+	 * 2-3 끝
+	 * */
+	public static void main(String[] args) {
+		
+				
+	}
+
+}
